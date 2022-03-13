864cc4b2837b8106f95714199db1f3f37fc338d5
DeMesaCharedel <charedeldemesa631@gmail.com>
2022-03-13 14:43:24 +0800

    My Personal Info added

---
+++ git-python-demesa.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 13 14:40:40 2022
+
+@author: TOSHIBA PC
+"""
+
+print ("Charedel Obando De Mesa");
+print ("Tabigue Boac Marinduque");
+print ("February 16, 2002");

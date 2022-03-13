32a62852df7c9c192a1370bc10002a56e9e41e5d
DeMesaCharedel <charedeldemesa631@gmail.com>
2022-03-13 15:05:27 +0800

    My Educational Background added

--- git-python-demesa.py
+++ git-python-demesa.py
@@ -8,3 +8,6 @@ Created on Sun Mar 13 14:40:40 2022
 print ("Charedel Obando De Mesa");
 print ("Tabigue Boac Marinduque");
 print ("February 16, 2002");
+print ("Marinduque State College");
+print ("Bachelor of Science in Information Technology");
+print ("BSI/T-2B")
